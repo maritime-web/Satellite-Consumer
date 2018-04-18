2a9e6dc4b2bc2685604c300b3db7cab29b0e2840
Oliver Steensen-Bech Haagh <osh@dma.dk>
2018-04-18 15:47:33 +0200

    disable downloading for Greenland and start downloading for Ghana

--- consume.sh
+++ consume.sh
@@ -14,35 +14,35 @@ function checkFileSizes {
 }
 suffixterra="latest.terra.250m.tif"
 suffixaqua="latest.aqua.250m.tif"
-places[0]="Academy_Glacier"
-places[1]="Geickie_Plateau"
-places[2]="Helheim_Area"
-places[3]="Humboldt_and_Petermann_Glacier"
-places[4]="JakobshavnIsbrae"
-places[5]="Kangerdlugssuaq"
-places[6]="Koge_Bugt"
-places[7]="Melville_Coast"
-places[8]="Northeast_Region"
-places[9]="North_Region"
-places[10]="Nuuk"
-places[11]="Scoresby_Sund"
-places[12]="Southeast_Region"
-places[13]="South_Region"
-places[14]="Southwest_Region"
-places[15]="Thule"
-places[16]="Uummannaq"
-cd /tmp
-# download for the coast of Greenland
-for i in "${places[@]}"
-do
-  curl -o "$i.$suffixterra" "https://lance.modaps.eosdis.nasa.gov/imagery/subsets/?project=arctic_regions&subset=$i.terra.250m.tif"
-  curl -o "$i.$suffixaqua" "https://lance.modaps.eosdis.nasa.gov/imagery/subsets/?project=arctic_regions&subset=$i.aqua.250m.tif"
-done
-checkFileSizes
-python -c "import os; os.chdir('/'); import CheckImages; CheckImages.checkImages('/tmp')"
-echo "Merging Arctic images"
-gdal_merge.py -o /data/Greenland.latest.terra.250m.tif *.terra.250m.tif
-gdal_merge.py -o /data/Greenland.latest.aqua.250m.tif *.aqua.250m.tif
+#places[0]="Academy_Glacier"
+#places[1]="Geickie_Plateau"
+#places[2]="Helheim_Area"
+#places[3]="Humboldt_and_Petermann_Glacier"
+#places[4]="JakobshavnIsbrae"
+#places[5]="Kangerdlugssuaq"
+#places[6]="Koge_Bugt"
+#places[7]="Melville_Coast"
+#places[8]="Northeast_Region"
+#places[9]="North_Region"
+#places[10]="Nuuk"
+#places[11]="Scoresby_Sund"
+#places[12]="Southeast_Region"
+#places[13]="South_Region"
+#places[14]="Southwest_Region"
+#places[15]="Thule"
+#places[16]="Uummannaq"
+#cd /tmp
+## download for the coast of Greenland
+#for i in "${places[@]}"
+#do
+#  curl -o "$i.$suffixterra" "https://lance.modaps.eosdis.nasa.gov/imagery/subsets/?project=arctic_regions&subset=$i.terra.250m.tif"
+#  curl -o "$i.$suffixaqua" "https://lance.modaps.eosdis.nasa.gov/imagery/subsets/?project=arctic_regions&subset=$i.aqua.250m.tif"
+#done
+#checkFileSizes
+#python -c "import os; os.chdir('/'); import CheckImages; CheckImages.checkImages('/tmp')"
+#echo "Merging Arctic images"
+#gdal_merge.py -o /data/Greenland.latest.terra.250m.tif *.terra.250m.tif
+#gdal_merge.py -o /data/Greenland.latest.aqua.250m.tif *.aqua.250m.tif
 cd /data
 # download for the Baltic Sea
 curl -o "BalticSea.$suffixterra" "https://lance.modaps.eosdis.nasa.gov/imagery/subsets/?subset=BalticSea.terra.250m.tif"
@@ -53,6 +53,9 @@ curl -o "AERONET_Hornsund.$suffixaqua" "https://lance.modaps.eosdis.nasa.gov/ima
 # download for Canada
 curl -o "ARCTAS_Spring_Canada.latest.terra.1km.tif" "https://lance.modaps.eosdis.nasa.gov/imagery/subsets/?subset=ARCTAS_Spring_Canada.terra.1km.tif"
 curl -o "ARCTAS_Spring_Canada.latest.aqua.1km.tif" "https://lance.modaps.eosdis.nasa.gov/imagery/subsets/?subset=ARCTAS_Spring_Canada.aqua.1km.tif"
+# download for Ghana
+curl -o "AERONET_Koforidua_ANUC.latest.terra.250m.tif" "https://lance-modis.eosdis.nasa.gov/imagery/subsets/?subset=AERONET_Koforidua_ANUC.terra.250m.tif"
+curl -o "AERONET_Koforidua_ANUC.latest.aqua.250m.tif" "https://lance-modis.eosdis.nasa.gov/imagery/subsets/?subset=AERONET_Koforidua_ANUC.aqua.250m.tif"
 checkFileSizes
 #python -c "import os; os.chdir('/'); import CheckImages; CheckImages.checkImages('/data')"
 exit 0
