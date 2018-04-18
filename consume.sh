92c0188268bb4229051ef7b9923da6c7a83f3fb2
Oliver Steensen-Bech Haagh <osh@dma.dk>
2018-04-18 15:42:51 +0200

    disable downloading images for Greenland and start downloading for Ghana

--- consume.sh
+++ consume.sh
@@ -13,31 +13,31 @@ function checkFileSizes {
   done
 }
 yesterday=$(date +%Y%j --date="1 day ago")
-places[0]="Academy_Glacier"
-places[1]="Geickie_Plateau"
-places[2]="Helheim_Area"
-places[3]="Humboldt_and_Petermann_Glacier"
-places[4]="JakobshavnIsbrae"
-places[5]="Kangerdlugssuaq"
-places[6]="Koge_Bugt"
-places[7]="Melville_Coast"
-places[8]="Northeast_Region"
-places[9]="North_Region"
-places[10]="Nuuk"
-places[11]="Scoresby_Sund"
-places[12]="Southeast_Region"
-places[13]="South_Region"
-places[14]="Southwest_Region"
-places[15]="Thule"
-places[16]="Uummannaq"
-cd /tmp
-# download for the coast of Greenland
-for i in "${places[@]}"
-do
-  curl -O -L -J "https://lance.modaps.eosdis.nasa.gov/imagery/subsets/?project=arctic_regions&subset=$i.$yesterday.terra.250m.tif"
-  curl -O -L -J "https://lance.modaps.eosdis.nasa.gov/imagery/subsets/?project=arctic_regions&subset=$i.$yesterday.aqua.250m.tif"
-done
-checkFileSizes
+#places[0]="Academy_Glacier"
+#places[1]="Geickie_Plateau"
+#places[2]="Helheim_Area"
+#places[3]="Humboldt_and_Petermann_Glacier"
+#places[4]="JakobshavnIsbrae"
+#places[5]="Kangerdlugssuaq"
+#places[6]="Koge_Bugt"
+#places[7]="Melville_Coast"
+#places[8]="Northeast_Region"
+#places[9]="North_Region"
+#places[10]="Nuuk"
+#places[11]="Scoresby_Sund"
+#places[12]="Southeast_Region"
+#places[13]="South_Region"
+#places[14]="Southwest_Region"
+#places[15]="Thule"
+#places[16]="Uummannaq"
+#cd /tmp
+## download for the coast of Greenland
+#for i in "${places[@]}"
+#do
+#  curl -O -L -J "https://lance.modaps.eosdis.nasa.gov/imagery/subsets/?project=arctic_regions&subset=$i.$yesterday.terra.250m.tif"
+#  curl -O -L -J "https://lance.modaps.eosdis.nasa.gov/imagery/subsets/?project=arctic_regions&subset=$i.$yesterday.aqua.250m.tif"
+#done
+#checkFileSizes
 python -c "import os; os.chdir('/'); import CheckImages; CheckImages.checkImages('/tmp')"
 echo "Merging Arctic images"
 gdal_merge.py -o /data/Greenland.$yesterday.terra.250m.tif *.terra.250m.tif
@@ -52,6 +52,9 @@ curl -O -L -J "https://lance.modaps.eosdis.nasa.gov/imagery/subsets/?project=aer
 # download for Canada
 curl -O -L -J "https://lance.modaps.eosdis.nasa.gov/imagery/subsets/?subset=ARCTAS_Spring_Canada.$yesterday.terra.1km.tif"
 curl -O -L -J "https://lance.modaps.eosdis.nasa.gov/imagery/subsets/?subset=ARCTAS_Spring_Canada.$yesterday.aqua.1km.tif"
+# download for Ghana
+curl -O -L -J "https://lance-modis.eosdis.nasa.gov/imagery/subsets/?subset=AERONET_Koforidua_ANUC.$yesterday.terra.250m.tif"
+curl -O -L -J "https://lance-modis.eosdis.nasa.gov/imagery/subsets/?subset=AERONET_Koforidua_ANUC.$yesterday.aqua.250m.tif"
 checkFileSizes
 python -c "import os; os.chdir('/'); import CheckImages; CheckImages.checkImages('/data')"
 exit 0
